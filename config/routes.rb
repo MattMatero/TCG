ea65e633ab05704c03394077df4fada5046c77f8
Matt Matero <matt@matero.me>
2015-04-16 18:02:54 -0400

    profiles, user table username col, profile button

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
 
   resources :cards
 
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
 
   root 'home#index'
 
   get 'home/contact'
 
   get 'home/about'
+
+  get 'home/profile'
 
   get 'cards/office'
  
